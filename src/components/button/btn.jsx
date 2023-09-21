dc9215b442691bc3e6b1051e5754c262be077f42
AnastasiaLeo <anas-leonidovna@mail.ru>
2023-09-21 16:55:49 +0700

    btn class->function

--- src/components/button/btn.jsx
+++ src/components/button/btn.jsx
@@ -1,39 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './btn.css';
 import Input from '../input/input.jsx';
 
-class Btn extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            clicked: false,
-        }
-    }
-    handleClick = () => {
-        console.log(this.state.clicked);
-        if (this.state.clicked == true){
-            this.setState({
-                clicked: false,
-            })
-        } else {
-            this.setState({
-                clicked: true,
-            })
-        }
-    }
+function Btn ( props ){
+    const [ click, setClick ] = useState(false)
 
-    render(){
-        const clicked = this.state.clicked;
-        return(
-            <div>
-                {/* <Input /> */}
-                {clicked ? <Input /> : ''}
-                <button className={clicked ? "btn btn-submit" : "btn"} onClick={this.handleClick}>
-                    {clicked ? 'Submit' : '+ Add cart'}
-                </button>
-            </div>
-        )
-    }
+    useEffect( () => {
+        click = setClick( !click )
+        console.log(click);
+    }, [ click])
+
+    return(
+        <div>
+            {/* <Input /> */}
+            {click ? <Input /> : ''}
+            <button className={click ? "btn btn-submit" : "btn"} onClick={this.handleClick}>
+                   {click ? 'Submit' : '+ Add cart'}
+            </button>
+        </div>
+    )
 }
+
+// class Btn extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             clicked: false,
+//         }
+//     }
+//     handleClick = () => {
+//         //console.log(this.state.clicked);
+//         this.setState({ clicked: !this.state.clicked })
+//     }
+
+//     render(){
+//         const clicked = this.state.clicked;
+//         return(
+//             <div>
+//                 {/* <Input /> */}
+//                 {clicked ? <Input /> : ''}
+//                 <button className={clicked ? "btn btn-submit" : "btn"} onClick={this.handleClick}>
+//                     {clicked ? 'Submit' : '+ Add cart'}
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
 
 export default Btn;
